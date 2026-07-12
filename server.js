6a5aea52419377ed02627f0802805ae053f2c04f
mvpcyber <mvpcyberjakarta@gmail.com>
2026-07-13 04:49:14 +0700

    update2

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-import './server/index.js';
+import { createServer } from "http";
+import next from "next";
+
+const port = Number(process.env.PORT || 3000);
+const hostname = "0.0.0.0";
+
+const app = next({
+  dev: false,
+  hostname,
+  port,
+});
+
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    handle(req, res);
+  }).listen(port, hostname, () => {
+    console.log(`Next.js server ready on http://${hostname}:${port}`);
+  });
+});
